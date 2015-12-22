00aad1909cfb67cbc5c3d62c2828d8e24a975846
Hendrik Coetzee <hs.coetzee@gmail.com>
2015-12-22 22:27:50 +0200

    "No" is going to quit page, "Yes" destination does not exist.
    Not to mention still need to implement all the state checking.
    And implement the state.

--- src/main/java/net/pepperbytes/controllers/StartController.java
+++ src/main/java/net/pepperbytes/controllers/StartController.java
@@ -3,6 +3,7 @@ package net.pepperbytes.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StartController {
@@ -10,6 +11,14 @@ public class StartController {
     @RequestMapping(value = {"/"} , method = RequestMethod.GET)
     public String getStartPage () {
         return "start";
+    }
+    
+    @RequestMapping("/startGame" )
+    public String startGame (@RequestParam(value="a", required=true, defaultValue="n") String answer) {
+        if ("y".equalsIgnoreCase(answer)) {
+            return "play";
+        }
+        return "goodbye";
     }
 
 }
